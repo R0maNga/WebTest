a68c0d151c8f760565a797c2bec535f7368d2cc7
R0maNga <R0maNga@users.noreply.github.com>
2022-11-15 19:36:21 +0300

    Добавьте файлы проекта.

---
+++ WebTest/Shared/Automapper/ClothesProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using DAL.Models.ForClothes.Request;
+using DAL.Models.ForClothes.Response;
+
+namespace DAL.Automapper;
+
+public class ClothesProfile
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<GetClothesById, CreateClothes>();
+            CreateMap<GetClothesById, UpdateClothes>();
+            CreateMap<GetClothesById, DeleteClothes>();
+        }
+    }
+}
